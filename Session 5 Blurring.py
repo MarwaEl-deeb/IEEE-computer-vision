f9b31e8b7b2f64122f0f3c4da4259ecdbd0b4e9a
MarwaEldeeb <125557487+MarwaEl-deeb@users.noreply.github.com>
2024-03-15 19:04:41 +0200

    Update Session 5 Blurring.py

--- Session 5 Blurring.py
+++ Session 5 Blurring.py
@@ -1,32 +1,124 @@
 import cv2
 """
-#####################################
-        Images Noting Logic
-#####################################
+#######################################################
+      Blurring techniques without using NumPy
+#######################################################
 """
-# Reading images
-white = cv2.imread("Images\white.png")
-bridge = cv2.imread("Images\Bridge.jpeg")
-tree = cv2.imread("Images\Tree.jpg")
+bug = cv2.imread("Blur images/bug.jpg")
 
-# Resizing images
-r_white = cv2.resize(white, (600, 400))
-r_tree = cv2.resize(tree, (600, 400))
-r_bridge = cv2.resize(bridge, (600, 400))
+# Average Blur
+avg1 = cv2.blur(bug, (3,3))
+avg2 = cv2.blur(bug, (5,5))
+avg3 = cv2.blur(bug, (3,5))
+avg4 = cv2.blur(bug, (7,7))
+cv2.imshow("Original bug", bug)
+cv2.imshow("Average blur 3", avg1)
+cv2.imshow("Average blur 5", avg2)
+cv2.imshow("Average blur 3*5", avg3)
+cv2.imshow("Average blur 7", avg4)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# Subtracting from white image
-sbct_tree = cv2.subtract(r_white, r_tree)
-sbct_bridge = cv2.subtract(r_white, r_bridge)
+# Gaussian Blur
+gausin1 = cv2.GaussianBlur(bug, (3,3), 0)
+gausin2 = cv2.GaussianBlur(bug, (5,5), 0)
+gausin3 = cv2.GaussianBlur(bug, (3,5), 0)
+gausin4 = cv2.GaussianBlur(bug, (7,7), 0)
+cv2.imshow("Original bug", bug)
+cv2.imshow("Gaussian blur 3", gausin1)
+cv2.imshow("Gaussian blur 5", gausin2)
+cv2.imshow("Gaussian blur 3*5", gausin3)
+cv2.imshow("Gaussian blur 7", gausin4)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-cv2.imshow("Noted tree by white image", sbct_tree)
-cv2.imshow("Noted bridge by white image", sbct_bridge)
+# Median Blur
+median1 = cv2.medianBlur(bug, 3)
+median2 = cv2.medianBlur(bug, 5)
+median3 = cv2.medianBlur(bug, 7)
+median4 = cv2.medianBlur (bug, 9)
+cv2.imshow("Original bug", bug)
+cv2.imshow("Median blur 3", median1)
+cv2.imshow("Median blur 5", median2)
+cv2.imshow("Median blur 7", median3)
+cv2.imshow("Median blur 9", median4)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#Bilateral Blur
+bilaterial1 = cv2.bilateralFilter(bug, 5, 75, 75)
+bilaterial2 = cv2.bilateralFilter(bug, 9, 75, 75)
+bilaterial3 = cv2.bilateralFilter(bug, 9, 95, 95)
+bilaterial4 = cv2.bilateralFilter(bug, 11, 75, 75)
+cv2.imshow("Original bug", bug)
+cv2.imshow("Bilateral blur 5", bilaterial1)
+cv2.imshow("Bilateral blur 9 75", bilaterial2)
+cv2.imshow("Bilateral blur 9 95", bilaterial3)
+cv2.imshow("Bilateral blur 11", bilaterial4)
 cv2.waitKey(0)
 
 
-# NOTing images
-not_tree = cv2.bitwise_not(r_tree)
-not_bridge = cv2.bitwise_not(r_bridge)
+"""
+####################################################
+      Blurring techniques using NumPy
+####################################################
+"""
+import numpy as np
+bug = cv2.imread("Blur images/bug.jpg")
+eye = cv2.imread("Blur images/eye.jpg")
+photo = cv2.imread("Blur images/photographer.jpg")
 
-cv2.imshow("NOTed Tree", not_tree)
-cv2.imshow("NOTed Bridge", not_bridge)
-cv2.waitKey(0)
+# Average Blur
+def avg_blur(image):
+    average = np.hstack([image,
+                         cv2.blur(image, (3, 3)),
+                         cv2.blur(image, (3, 5)),
+                         cv2.blur(image, (5, 5)),
+                         cv2.blur(image, (7, 7))])
+    cv2.imshow("Average Blur", average)
+    cv2.waitKey(0)
+
+# Gaussian Blur
+def gaussian_blur(image):
+    gaussian = np.hstack([image,
+                          cv2.GaussianBlur(image, (3, 3), 0),
+                          cv2.GaussianBlur(image, (3, 5), 0),
+                          cv2.GaussianBlur(image, (5, 5), 0),
+                          cv2.GaussianBlur(image, (7, 7), 0)])
+    cv2.imshow("Gaussian Blur", gaussian)
+    cv2.waitKey(0)
+
+# Median Blur
+def median_blur(image):
+    median = np.hstack([image,
+                        cv2.medianBlur(image, 3),
+                        cv2.medianBlur(image, 5),
+                        cv2.medianBlur(image, 7),
+                        cv2.medianBlur(image, 9)])
+    cv2.imshow("Median Blur", median)
+    cv2.waitKey(0)
+
+#Bilateral Blur
+def bilateral_blur(image):
+    bilateral = np.hstack([image,
+                           cv2.bilateralFilter(image, 3, 75, 75),
+                           cv2.bilateralFilter(image, 5, 75, 75),
+                           cv2.bilateralFilter(image, 7, 75, 75),
+                           cv2.bilateralFilter(image, 9, 75, 75)])
+    cv2.imshow("Bilateral Blur", bilateral)
+    cv2.waitKey(0)
+
+avg_blur(bug)
+gaussian_blur(bug)
+median_blur(bug)
+bilateral_blur(bug)
+
+avg_blur(eye)
+gaussian_blur(eye)
+median_blur(eye)
+bilateral_blur(eye)
+
+avg_blur(photo)
+gaussian_blur(photo)
+median_blur(photo)
+bilateral_blur(photo)
